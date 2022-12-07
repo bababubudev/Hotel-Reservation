b8ed712f7af04456b3c8a002ab5b401bc0e6213b
bababubudev <buttblaster01@gmail.com>
2022-12-07 04:01:49 +0200

    Feature to cancel booking and bug fixes.

--- Hotel Task.cpp
+++ Hotel Task.cpp
@@ -9,6 +9,7 @@ struct Reservation
 {
 	std::string reserve_name = std::string();
 	
+	bool is_set = false;
 	bool is_booked = false;
 	bool is_double = false;
 
@@ -18,7 +19,17 @@ struct Reservation
 	double current_cost = 0.0;
 };
 
-int fetch_room(Reservation* reserves, bool want_doubles)
+int fetch_type()
+{
+	std::cout << "\nSelect room type!\n";
+	std::cout << "1. [ Single room ]\n";
+	std::cout << "2. [ Double room ]\n";
+	std::cout << "3. [ Go to start ]\n\n";
+
+	return validated_input<int>(1, 3);
+}
+
+int fetch_room(Reservation *reserves, bool want_doubles)
 {
 	int index = 0;
 
@@ -43,7 +54,7 @@ int fetch_room(Reservation* reserves, bool want_doubles)
 	return index;
 }
 
-void calculate_prices(Reservation& room, bool log)
+void fetch_prices(Reservation &room, bool log)
 {
 	int cost = room.is_double ? PRICE_D * room.nights : PRICE_S * room.nights;
 	int discount = discount_amount();
@@ -60,106 +71,22 @@ void calculate_prices(Reservation& room, bool log)
 	room.current_cost = cost - ((double)cost * discount * 0.01);
 }
 
-#pragma region Input Validation
-
-template<typename T>
-void validated_input(T& input, int min = 0, int max = 0)
-{
-	std::cin >> std::setw(1) >> input;
-	bool comparable = !(min == max);
-	bool in_between = input >= min && input <= max;
-
-	while (!std::cin.good() || (!in_between && comparable))
-	{
-		if (std::cin.good())
-		{
-			std::cout << "\n[ Only numbers from " << min << " : " << max << " are valid! ]\n\n";
-
-			std::cin >> std::setw(1) >> input;
-			in_between = input >= min && input <= max;
-		}
-		else
-		{
-			std::cin.clear();
-			std::cin.ignore(INT_MAX, '\n');
-
-			std::cout << "\n[ Only numbers are allowed! ]\n\n";
-
-			std::cin >> std::setw(1) >> input;
-			in_between = input >= min && input <= max;
-		}
-	}
-
-	std::cin.clear();
-	std::cin.ignore(INT_MAX, '\n');
-}
-
-template<typename T>
-T validated_input(int min = 0, int max = 0)
-{
-	T input;
-	validated_input<T>(input, min, max);
-
-	return input;
-}
-
-void validated_input(std::string& input, bool num_allowed = false)
-{
-	int checker = 0;
-	bool success = false;
-
-	std::getline(std::cin, input);
-
-	if (num_allowed) return;
-
-	while (!success)
-	{
-		try
-		{
-			checker = std::stoi(input);
-			std::cout << "\n[ Name cannot start with a number ]\n\n";
-			std::getline(std::cin, input);
-		} 
-		catch (const std::exception&)
-		{
-			if (input == "")
-			{
-				std::cout << "\n[ Please enter your name before you continue ]\n\n";
-				std::cin.clear();
-				std::getline(std::cin, input);
-				continue;
-			}
-
-			success = true;
-		}
-	}
-}
-
-std::string validated_input(bool num_allowed = false)
-{
-	std::string input;
-	validated_input(input, num_allowed);
-
-	return input;
-}
-
-#pragma endregion
-
 #pragma region Logs
 
 void log_choices()
 {
 	std::cout << "What's on your mind?\n";
 	std::cout << "1. [ Reserve a room ]\n";
-	std::cout << "2. [ Your reservations ]\n";
-	std::cout << "3. [ Quit application ]\n\n";
+	std::cout << "2. [ Your reservation ]\n";
+	std::cout << "3. [ Cancel reservation ]\n";
+	std::cout << "4. [ Quit application ]\n\n";
 }
 
 void log_reserved(Reservation &current_room, bool foreign = false)
 {
 	const char* type = current_room.is_double ? "Double" : "Single";
 
-	if (!foreign) std::cout << "\n[ Your room has been booked " << current_room.reserve_name << " ]\n";
+	if (!foreign) std::cout << "\nYour room has been booked " << current_room.reserve_name << "\n";
 
 	std::cout << "--------------------------\n";
 	std::cout << "Name: [" << current_room.reserve_name << "]\n";
@@ -167,14 +94,16 @@ void log_reserved(Reservation &current_room, bool foreign = false)
 	std::cout << "Nights: [" << current_room.nights << "]\n";
 	std::cout << "ID: [" << current_room.reservation_id << "]\n\n";
 
-	calculate_prices(current_room, !foreign);
+	fetch_prices(current_room, !foreign);
 	std::cout << "Total cost: [" << current_room.current_cost << " EUR]\n";
 	std::cout << "--------------------------\n\n";
 }
 
-void log_rooms(Reservation * rooms)
+void log_all(Reservation *rooms)
 {
 	std::cout << "\n";
+	bool found = false;
+
 	for (int i = 0; i < MAX_ROOMS; i++)
 	{
 		if (!rooms[i].is_booked) continue;
@@ -183,30 +112,23 @@ void log_rooms(Reservation * rooms)
 
 		if (rooms[i].is_double)
 		{
-			std::cout << i << ". [ Double ] Booked by: " << teller << "\n";
-			
+			std::cout << i << ". [ Double ] Booked by: " << teller << ". [" << rooms[i].reservation_id << "]\n";
+			found = true;
 		}
 		else
 		{
-			std::cout << i << ". [ Single ] Booked by: " << teller << "\n";
+			std::cout << i << ". [ Single ] Booked by: " << teller << ". [" << rooms[i].reservation_id << "]\n";
+			found = true;
 		}
 	}
-	std::cout << "\n";
+
+	const char* teller = found ? "\n" : "[ No booked rooms found! ]\n\n";
+	std::cout << teller;
 }
 
 #pragma endregion
 
-int room_type()
-{
-	std::cout << "\nSelect room type!\n";
-	std::cout << "1. [ Single room ]\n";
-	std::cout << "2. [ Double room ]\n";
-	std::cout << "3. [ Go to start ]\n\n";
-	
-	return validated_input<int>(1, 3);
-}
-
-void book_room(Reservation& room)
+void book_room(Reservation &room)
 {
 	if (room.is_booked)
 	{
@@ -218,6 +140,7 @@ void book_room(Reservation& room)
 	validated_input(room.nights, 1, 365);
 
 	room.is_booked = true;
+	room.is_set = true;
 	room.reservation_id = reserve_id();
 
 	std::cout << "\n[ Specify the reservor's name ]\n\n";
@@ -226,82 +149,140 @@ void book_room(Reservation& room)
 	log_reserved(room);
 }
 
-void check_reservation(Reservation* reserves)
+void remove_room(Reservation &room)
 {
-	short int found_index[MAX_ROOMS];
+	std::cout << "\nAre you sure you want to cancel this reservation " << room.reserve_name << "?\n";
+	std::cout << "1. [ Yes! Cancel reservation! ]\n";
+	std::cout << "2. [ No! Take me back! ]\n\n";
+
+	int choice = validated_input<int>(1, 2);
+
+	switch (choice)
+	{
+	case 1:
+		std::cout << "\n[ Reservation for " << room.reserve_name << " with ID " << room.reservation_id << " is now cancelled! ]\n\n";
+		room.is_booked = false;
+		room.reserve_name = std::string();
+		room.reservation_id = -1;
+		room.nights = 0;
+		room.current_cost = 0.0;
+		break;
+	case 2:
+		std::cout << "\n";
+		return;
+	}
+}
+
+void release_room(Reservation *reserves, int &singles, int &doubles)
+{
+	int ind_d = fetch_room(reserves, true);
+	int ind_s = fetch_room(reserves, false);
+
+	if (!reserves[ind_d].is_booked && reserves[ind_d].is_set)
+	{
+		printf("\n%i released.\n\n", ind_d);
+		doubles++;
+	}
+
+	if (!reserves[ind_s].is_booked && reserves[ind_s].is_set)
+	{
+		printf("\n%i released.\n\n", ind_s);
+		singles++;
+	}
+}
+
+void manage_reservation(Reservation *reserves, bool cancellation = false)
+{
+	int found_index[MAX_ROOMS];
 
 	int id = 0;
-	short int indexer = 0;
+	int amount = 0;
 	bool is_str = false;
 
-	std::cout << "\n[ Search for reservee name or the room id ]\n\n";
+	const char* teller = cancellation ? "cancel" : "check";
+
+	std::cout << "\n[ To " << teller << ", enter Room ID or Reservee's name ]\n\n";
 	std::string r_info = validated_input(true);
 
 	try
 	{
 		id = std::stoi(r_info);
 		is_str = false;
-	} 
+	}
 	catch (const std::exception&)
 	{
 		is_str = true;
 	}
 
-
 	for (int i = 0; i < MAX_ROOMS; i++)
 	{
-		if (!reserves[i].is_booked || reserves[i].reservation_id == -1) continue;
+		if (!reserves[i].is_booked) continue;
 
 		if (is_str)
 		{
 			if (r_info == reserves[i].reserve_name)
 			{
-				found_index[indexer] = i;
-				indexer++;
+				found_index[amount] = i;
+				amount++;
 			}
 		}
 		else
 		{
 			if (id == reserves[i].reservation_id)
 			{
-				found_index[indexer] = i;
-				indexer++;
+				found_index[amount] = i;
+				amount++;
 			}
 		}
 	}
 
-	if (indexer == 0)
+	if (amount == 0)
 	{
 		std::cout << "\n[ " << r_info << " ] not found in the system!\n\n";
 		return;
 	}
-
-	if (!is_str && indexer > 1)
+		
+	if (!is_str && amount > 1)
 	{
-		Reservation reservee = reserves[found_index[0]];
+		Reservation& reservee = reserves[found_index[0]];
 		reservee.reservation_id = id + 1;
-		std::cout << "\n[ Something went wrong! Reservee " << reservee.reserve_name <<"'s id was renewed to " << reservee.reservation_id << " ]\n\n";
+		std::cout << "\n[ Something went wrong! Reservee " << reservee.reserve_name << "'s id was renewed to " << reservee.reservation_id << " ]\n\n";
 		return;
 	}
 
+	//if (cancellation) return;
+
 	std::cout << "\nRoom booked by [" << r_info << "]\n";
-	for (int i = 0; i < indexer; i++)
+	for (int i = 0; i < amount; i++)
 	{
 		log_reserved(reserves[found_index[i]], true);
 	}
+
+	if (cancellation)
+	{
+		int choice = 0;
+		if (amount > 1)
+		{
+			std::cout << "\n[ " << amount << " rooms found in your reservations! Please enter the corresponding number for the list above! ]\n\n";
+			validated_input<int>(choice, 1, amount);
+			choice -= 1;
+		}
+
+		remove_room(reserves[found_index[choice]]);
+	}
 }
 
-void controller(Reservation * reserves, int &single_am, int &double_am, bool &wants_to_quit)
+void controller(Reservation *reserves, int &single_am, int &double_am, bool &wants_to_quit)
 {
 	printf("AVAILABLE ROOMS\nsingle: %i, double: %i\n\n", single_am, double_am);
 	log_choices();
 
-	int current_choice = validated_input<int>(1, 4);
+	int current_choice = validated_input<int>(1, 5);
 
 	switch (current_choice)
 	{
 	case 1:
-		switch (room_type())
+		switch (fetch_type())
 		{
 		case 1:
 			std::cout << "\n";
@@ -320,26 +301,31 @@ void controller(Reservation * reserves, int &single_am, int &double_am, bool &wa
 		}
 		break;
 	case 2:
-		check_reservation(reserves);
+		manage_reservation(reserves);
 		break;
 	case 3:
-		wants_to_quit = true;
+		manage_reservation(reserves, true);
+		release_room(reserves, single_am, double_am);
 		break;
 	case 4:
-		log_rooms(reserves);
+		wants_to_quit = true;
+		break;
+	case 5:
+		log_all(reserves);
 		break;
 	}
+		
 }
 
 int main()
 {
 	bool wants_to_quit = false;
-	int booked_amount = 0;
 	
 	int r_division = room_division() * 2;
 	int single_room = r_division / 2, double_room = r_division / 2;
 
 	Reservation user_reservation[MAX_ROOMS];
+
 	for (int i = 0; i < double_room; i++)
 	{
 		user_reservation[single_room + i].is_double = true;
@@ -349,7 +335,10 @@ int main()
 	{
 		controller(user_reservation, single_room, double_room, wants_to_quit);
 	}
-	while (booked_amount < (single_room + double_room) && !wants_to_quit);
+	while (!wants_to_quit);
+
+	//TODO: Restrict from reserving unavailable rooms. Stop appropriately when rooms are full.
+	//TODO: 
 
 	return 0;
 }
--- Utility.h
+++ Utility.h
@@ -1,7 +1,6 @@
 #pragma once
 #include <cstdlib>
 #include <ctime>
-#include "Constants.h"
 
 int reserve_id()
 {
@@ -20,3 +19,88 @@ int room_division()
 	srand((int)time(NULL));
 	return rand() % (MAX_ROOMS / DIVISIONS) + MIN_ROOMS;
 }
+
+#pragma region Input Validation
+
+template<typename T>
+void validated_input(T& input, int min = 0, int max = 0)
+{
+	std::cin >> std::setw(1) >> input;
+	bool comparable = !(min == max);
+	bool in_between = input >= min && input <= max;
+
+	while (!std::cin.good() || (!in_between && comparable))
+	{
+		if (std::cin.good())
+		{
+			std::cout << "\n[ Only numbers from " << min << " : " << max << " are valid! ]\n\n";
+
+			std::cin >> std::setw(1) >> input;
+			in_between = input >= min && input <= max;
+		}
+		else
+		{
+			std::cin.clear();
+			std::cin.ignore(INT_MAX, '\n');
+
+			std::cout << "\n[ Only numbers are allowed! ]\n\n";
+
+			std::cin >> std::setw(1) >> input;
+			in_between = input >= min && input <= max;
+		}
+	}
+
+	std::cin.clear();
+	std::cin.ignore(INT_MAX, '\n');
+}
+
+template<typename T>
+T validated_input(int min = 0, int max = 0)
+{
+	T input;
+	validated_input<T>(input, min, max);
+
+	return input;
+}
+
+void validated_input(std::string& input, bool num_allowed = false)
+{
+	int checker = 0;
+	bool success = false;
+
+	std::getline(std::cin, input);
+
+	if (num_allowed) return;
+
+	while (!success)
+	{
+		try
+		{
+			checker = std::stoi(input);
+			std::cout << "\n[ Name cannot start with a number ]\n\n";
+			std::getline(std::cin, input);
+		}
+		catch (const std::exception&)
+		{
+			if (input == "")
+			{
+				std::cout << "\n[ Please enter your name before you continue ]\n\n";
+				std::cin.clear();
+				std::getline(std::cin, input);
+				continue;
+			}
+
+			success = true;
+		}
+	}
+}
+
+std::string validated_input(bool num_allowed = false)
+{
+	std::string input;
+	validated_input(input, num_allowed);
+
+	return input;
+}
+
+#pragma endregion
